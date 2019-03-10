cf0b95a2d147f30b86059a94e80b8f7cd1a42f5f
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-10 09:12:29 -0700

    hooks: passes props to ResourceList comp

--- src/hooks/Hooks.js
+++ src/hooks/Hooks.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import ResourceList from "./ResourceList";
 
 const Hooks = () => {
   const [resource, setResource] = useState("posts");
@@ -8,7 +9,7 @@ const Hooks = () => {
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 };
