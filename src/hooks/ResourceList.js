20a19babe9d7adbaafdd2298ea2125ac885efe71
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-10 22:30:57 -0700

    hooks: useEffect

--- src/hooks/ResourceList.js
+++ src/hooks/ResourceList.js
@@ -1,15 +1,24 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-const ResourceList = () => {
+const ResourceList = ({resource}) => {
+  // any time ResourceList first gets rendered, or receives a new value for resource (posts/todos),
+  // we want to call fetchResource, then eventually update our state
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async () => {
+  // useEffect allows us to use lifecycle methods in a func comp
+  // useEffect essentially combines CDM and CDU
+
+  const fetchResource = async resource => {
     const response = await axios.get(
-      `https://http://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
-    this.setState = {resources: response.data};
+    setResources(response.data);
   };
+
+  useEffect(() => {
+    fetchResource(resource);
+  }, []);
 
   return <div>{resources.length}</div>;
 };
