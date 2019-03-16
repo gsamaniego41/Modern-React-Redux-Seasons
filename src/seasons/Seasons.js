d6747ec65c84e19dd614743f82f8e89132faabe4
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-15 23:27:02 -0700

    hooks: refactors seasons from class to functional comp

--- src/seasons/Seasons.js
+++ src/seasons/Seasons.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SeasonDisplay from "./SeasonDisplay";
 import Loader from "./Loader";
 
+const Seasons = () => {
+  // state = {lat: null, errorMessage: ''}   --> useState
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat({lat: position.coords.latitude}),
+      err => setErrorMessage({errorMessage: err.message})
+    );
+  }, []);
+  /* empty array means only run this function one time (in total)
+   for the entire lifecycle of this component */
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Loader message="Please accept location request" />;
+  }
+
+  return <div className="border red">{content}</div>;
+};
+
+/* 
 class Seasons extends React.Component {
-  state = {lat: null, errorMessage: ""};
+  // state = {lat: null, errorMessage: ""}; --> useState
 
   componentDidMount = () => {
     window.navigator.geolocation.getCurrentPosition(
@@ -25,6 +52,8 @@ class Seasons extends React.Component {
   render() {
     return <div className="border red">{this.renderContent()}</div>;
   }
-}
+} 
+
+*/
 
 export default Seasons;
